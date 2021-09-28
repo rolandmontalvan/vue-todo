0a6a81f37171627143d96288ada6851837579d47
rolandmontalvan <rolandfilho@gmail.com>
2021-09-28 13:33:25 -0300

    pronto para producao

---
+++ src/store/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    tarefas:[
+      {id:1, titulo:"Comprar cachaça", concluido:false},
+      {id:2, titulo:"Pegar mais cerveja", concluido:false},
+    ]
+  },
+  mutations: {
+    adicionaTarefa(state, titulo){
+      if(titulo){
+        state.tarefas.push({
+          id: new Date().getTime(),
+          titulo,
+          concluido: false
+        })
+      }
+    },
+    removeTarefa(state, id){
+      state.tarefas = state.tarefas.filter(tarefa => tarefa.id !== id)
+    }
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
